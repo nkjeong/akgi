85d1c1878e4f4225465b03331decc52d68106d72
ahtty <ahtty@DESKTOP-8QUNMCK>
2023-09-25 20:13:57 +0900

    akgi

---
+++ web/src/main/resources/static/script/index.js
@@ -0,0 +1,68 @@
+"use strict";
+
+const getCategory = async () => {
+    try {
+        const response = await fetch('/categories');
+        
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        
+        const data = await response.json();
+        setCategory(data);
+    } catch (error) {
+        console.error('Fetch error:', error);
+    }
+};
+
+const setCategory = (data) => {
+    const mainNavigationElement = document.querySelector('section.mainNav ul');
+    const html = data.map(d => `
+        <li>
+            <article>${d.name}</article>
+            <article class="subMenu" data-code="${d.code}">
+                <ul></ul>
+            </article>
+        </li>
+    `).join('');
+
+    mainNavigationElement.innerHTML = html;
+    setSubMenu(mainNavigationElement);
+};
+
+// Initiate the getCategory function
+getCategory();
+
+const setSubMenu = (element) => {
+	const getSubMenuExpectedElement = element.querySelectorAll('article.subMenu');
+	getSubMenuExpectedElement.forEach(ele => {
+		subMenu(ele.dataset.code, ele);
+	})
+}
+
+const getCategoriesByCategoryCode = async (categoryCode) => {
+    try {
+        const response = await fetch(`/categoryone/category/${categoryCode}`);
+        
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Fetch error:', error);
+    }
+};
+
+const subMenu = async (categoryCode, ele) => {
+    const categories = await getCategoriesByCategoryCode(categoryCode);
+    const getSubMenuSetElement = ele.querySelector('ul');
+    let html = '';
+    categories.forEach(category => {
+		html += `
+			<li>${category.name}</li>
+		`;
+    });
+    getSubMenuSetElement.innerHTML = html;
+};
