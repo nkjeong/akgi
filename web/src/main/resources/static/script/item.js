f6202f860cb8cbcb75b784d5ffe59ae1caa59c21
nkjeong <abidan@naver.com>
2023-09-28 19:37:35 +0900

    akgi

--- web/src/main/resources/static/script/item.js
+++ web/src/main/resources/static/script/item.js
@@ -1,54 +1,42 @@
 "use strict";
 
-const getCategoryItem = (categoryPrefix, name) => {
-	const listTitle = document.querySelector('.listCategory .listTitle span.itemSize');
-	fetch(`/api/gallery/category/${categoryPrefix}`)
-		.then(response => {
-			if (!response.ok) {
-				throw new Error('Network response was not ok');
-			}
-			return response.json();
-		})
-		.then(data => {
-			listTitle.innerHTML = `
-				<b>${name}(${data.length}개)</b>
-			`;
-			setList(data);
-		})
-		.catch(error => {
-			console.log('There was a problem with the fetch operation:', error.message);
-		});
+const getCategoryItem = async (categoryPrefix, name) => {
+    const listTitle = document.querySelector('.listCategory .listTitle span.itemSize');
+
+    try {
+        const response = await fetch(`/api/gallery/category/${categoryPrefix}`);
+        
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        
+        const data = await response.json();
+        listTitle.innerHTML = `<b>${name}(${data.length}개)</b>`;
+        setList(data);
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error.message);
+    }
+}
+
+const createItemHTML = (item) => {
+    let itemName = item.name.length > 10 ? item.name.substring(0, 11) + '...' : item.name;
+    return `
+        <section>
+            <article><img src="/images/1000/gransen_${item.code}.jpg"></article>
+            <article>${itemName}</article>
+            <article>${getCurrentMony(item.price)}</article>
+        </section>
+    `;
 }
 
 const setList = (data) => {
-	console.log(data)
-	const categoryList = document.querySelector('.listCategory .itemList');
-	let html = '';
-	let itemSize = 0;
-	if(data.length < 6){
-		itemSize = data.length;
-	}else{
-		itemSize = 6;
-	}
-	for(let i = 0 ; i < itemSize ; i++){
-		let itemName = data[i].name;
-		if(itemName.length > 10){
-			itemName = itemName.substring(0, 11)+'...';
-		}
-		html += `
-			<section>
-				<article><img src="/images/1000/gransen_${data[i].code}.jpg"></article>
-				<article>${itemName}</article>
-				<article>${getCurrentMony(data[i].price)}</article>
-			</section>
-		`;
-	}
-	categoryList.innerHTML = html;
+    const categoryList = document.querySelector('.listCategory .itemList');
+    const itemsToShow = data.slice(0, Math.min(data.length, 6));
+    categoryList.innerHTML = itemsToShow.map(createItemHTML).join('');
 }
 
 const setCategoryNumber = (categoryNumbers) => {
-	const number = getRandomNumber(1, categoryNumbers.length);
-	const code = categoryNumbers[number-1].code;
-	const name = categoryNumbers[number-1].name;
-	getCategoryItem(code, name);
+    const number = getRandomNumber(1, categoryNumbers.length);
+    const { code, name } = categoryNumbers[number - 1];
+    getCategoryItem(code, name);
 }
